440d9c44b978f444b19de0d6cbf4573e862e7f2b
Transcraft <aries1749@live.fr>
2016-11-13 15:44:37 +0100

    yop

--- c+c- v1/c+c- v1.cpp
+++ c+c- v1/c+c- v1.cpp
@@ -1,5 +1,5 @@
 // c+c- v1.cpp : Defines the entry point for the console application.
-//yop
+//
 
 #include "stdafx.h"
 #include <Windows.h>
@@ -15,7 +15,7 @@ using namespace std;
 
 int main()
 {
-	std::string neuronBase = "0:0#0!0,1,2,3,4,5|0:1#0!0,1,2,3,4,5|0:2#0!0,1,2,3,4,5|0:3#0!0,1,2,3,4,5|0:4#0!0,1,2,3,4,5|";
+	std::string neuronBase = "0:0#0!0|0:1#0!0|0:2#0!0|0:3#0!0|0:4#0!0|";
 	vector<IA> genom;
 
 	Game game;
